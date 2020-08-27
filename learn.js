38f3d8dcf2f02daa6c8110e423909631df71381b
SyedHamza <hamzasyed001230@gmail.com>
2020-08-28 04:39:29 +0500

    responsive nav sidebar and body :)

---
+++ learn.js
@@ -0,0 +1,10 @@
+function toggleNav() {
+  var navLength = document.getElementById("mySidebar");
+    if (navLength.style.width == "75px")
+    {
+      navLength.style.width = "250px";
+    }
+    else{
+      navLength.style.width = "75px";
+    }
+}
